cc9bfacebce0fc16ee59d01cfc3dcbdcc93c03ce
kingsubin <binch1226@naver.com>
2021-05-02 01:07:22 +0900

    first commit

---
+++ src/main/java/com/community/soob/SoobApplication.java
@@ -0,0 +1,13 @@
+package com.community.soob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/community/soob/SoobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.soob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
