4508883ab9cbe9dd61e63914da5129e30077742b
kingsubin <binch1226@naver.com>
2021-05-06 23:08:41 +0900

    ec2 테스트

--- src/main/java/com/community/soob/SoobApplication.java
+++ src/main/java/com/community/soob/SoobApplication.java
@@ -3,11 +3,17 @@ package com.community.soob;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Map;
+
+
 @SpringBootApplication
 public class SoobApplication {
 
     public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(SoobApplication.class);
+        springApplication.setDefaultProperties(Map.of("spring.profiles.active", "development"));
         SpringApplication.run(SoobApplication.class, args);
     }
-
 }
+
+// Ec2에서는 production 으로 돌림
