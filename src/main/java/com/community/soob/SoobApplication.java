7839c30211897c8c8c200ac8d7f2b1c9489830cc
kingsubin <binch1226@naver.com>
2021-05-07 19:47:51 +0900

    CI 에러 수정

--- src/main/java/com/community/soob/SoobApplication.java
+++ src/main/java/com/community/soob/SoobApplication.java
@@ -3,12 +3,14 @@ package com.community.soob;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Map;
+
 @SpringBootApplication
 public class SoobApplication {
 
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(SoobApplication.class);
-        springApplication.setAdditionalProfiles("development");
+        springApplication.setDefaultProperties(Map.of("spring.profiles.active", "development"));
         springApplication.run(args);
     }
 }
