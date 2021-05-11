84c529ad9cf061a684bfe4db1eec3e0fa87acd33
kingsubin <binch1226@naver.com>
2021-05-11 10:41:23 +0900

    CD TEST10

--- scripts/health.sh
+++ scripts/health.sh
@@ -1,17 +1,27 @@
+#!/usr/bin/env bash
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+source ${ABSDIR}/switch.sh
+
+IDLE_PORT=$(find_idle_port)
+
 echo "> Health Check Start!"
 echo "> IDLE_PORT: $IDLE_PORT"
-echo "> curl -s http://localhost:$IDLE_PORT/profile "
+echo "> curl -s http://localhost:$IDLE_PORT/actuator/health"
 sleep 10
 
-for RETRY_COUNT in {1..10}
+for RETRY_COUNT in {1..10}  # for문 10번 돌기
 do
-  RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
-  UP_COUNT=$(echo ${RESPONSE} | grep 'production' | wc -l)
+  echo "for문 진입"
+  RESPONSE=$(curl --connect-timeout 10 -s http://localhost:${IDLE_PORT}/actuator/health)   # 현재 문제 없이 잘 실행되고 있는 요청을 보내봅니다.
+  UP_COUNT=$(echo ${RESPONSE} | grep 'UP' | wc -l) # 해당 결과의 줄 수를 숫자로 리턴합니다.
 
   if [ ${UP_COUNT} -ge 1 ]
-  then # $up_count >= 1 ("real" 문자열이 있는지 검증)
+  then # $up_count >= 1 ("UP" 문자열이 있는지 검증)
       echo "> Health check 성공"
-      switch_proxy
+      switch_proxy   # switch.sh 실행
       break
   else
       echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
--- scripts/start.sh
+++ scripts/start.sh
@@ -14,26 +14,20 @@ pwd # debug
 
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
-
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
-
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)    # jar 이름 꺼내오기
-
 echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
-
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-
 IDLE_PROFILE=$(find_idle_profile)
 echo ${IDLE_PROFILE} # debug
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
-
 nohup java -jar $JAR_NAME \
     -Dspring.profiles.active=$IDLE_PROFILE \
     > $REPOSITORY/nohup.out 2>&1 &
