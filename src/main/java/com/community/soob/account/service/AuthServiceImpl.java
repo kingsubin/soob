8907d7ec5bc12d41ea5fe941b800b95bd49756da
kingsubin <binch1226@naver.com>
2021-05-24 17:24:23 +0900

    fix: checkRegex 메소드 수정
    - 기존에 오류 있던거를 되게끔 수정함

--- src/main/java/com/community/soob/account/service/AuthServiceImpl.java
+++ src/main/java/com/community/soob/account/service/AuthServiceImpl.java
@@ -124,7 +124,7 @@ public class AuthServiceImpl implements AuthService {
     public void checkRegex(String str) {
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            if (!Character.isDigit(c) || !Character.isLetter(c)) {
+            if (!Character.isLetterOrDigit(c)) {
                 throw new InvalidValueException();
             }
         }
