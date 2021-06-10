f0287ccf7503455b34055252cc5070b5a80c2a30
kingsubin <binch1226@naver.com>
2021-06-10 13:40:44 +0900

    test: PostServiceImplTest 추가
    - 조회, 생성, 수정, 삭제 테스트 추가

--- src/test/java/com/community/soob/service/PostServiceImplTest.java
+++ src/test/java/com/community/soob/service/PostServiceImplTest.java
@@ -1,10 +1,17 @@
 package com.community.soob.service;
 
+import com.community.soob.account.domain.Account;
+import com.community.soob.account.domain.Role;
 import com.community.soob.attachment.AttachmentService;
 import com.community.soob.comment.service.CommentService;
 import com.community.soob.heart.service.HeartService;
+import com.community.soob.post.domain.Board;
 import com.community.soob.post.domain.BoardRepository;
+import com.community.soob.post.domain.Post;
 import com.community.soob.post.domain.PostRepository;
+import com.community.soob.post.exception.AuthorNotEqualException;
+import com.community.soob.post.exception.BoardNotFoundException;
+import com.community.soob.post.exception.PostNotFoundException;
 import com.community.soob.post.service.PostServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +19,22 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.never;
 
 @ExtendWith(MockitoExtension.class)
 public class PostServiceImplTest {
@@ -22,65 +45,302 @@ public class PostServiceImplTest {
     @Mock private HeartService heartService;
     @Mock private AttachmentService attachmentService;
 
-    private final String directoryName = "post/";
+    private Account createAccount() {
+        return Account.builder()
+                .id(2L)
+                .email("test@test.com")
+                .password("$2a$10$2H.qwzvH9zq4NrqrGJWdZOVZ4nrx3rfgEqnKvK98fWvaop0ceVtt2")
+                .nickname("test")
+                .role(Role.NOT_PERMITTED)
+                .salt("$2a$10$2H.qwzvH9zq4NrqrGJWdZO")
+                .profileImage(null)
+                .build();
+    }
+
+    private Board createBoard() {
+        return Board.builder()
+                .id(1L)
+                .name("QNA")
+                .build();
+    }
+
+    private Post createPost() {
+        return Post.builder()
+                .id(1L)
+                .board(createBoard())
+                .author(createAccount())
+                .title("title1")
+                .content("content1")
+                .attachments(new ArrayList<>())
+                .readCount(0)
+                .heartCount(0)
+                .build();
+    }
 
     // ----- 게시글 조회 -----
     @DisplayName("게시글 조회 실패 - postId 존재하지 않음")
     @Test
     void testGetPostFailureByInvalidPostId() {
+        // given
+        long postId = 1L;
+
+        // when
+        // then
+        assertThrows(PostNotFoundException.class, () -> {
+            postServiceImpl.getPost(postId);
+        });
     }
 
     @DisplayName("게시글 조회 성공 - 조회수 증가")
     @Test
     void testGetPostSuccess() {
+        // given
+        long postId = 1L;
+        Post post = createPost();
+        given(postRepository.findById(postId)).willReturn(Optional.of(post));
+
+        // when
+        postServiceImpl.getPost(postId);
+
+        // then
+        assertEquals(1, post.getReadCount());
+        then(postRepository).should().save(post);
     }
 
     // ----- 게시글 작성 -----
     @DisplayName("게시글 작성 실패 - boardId 존재하지않음")
     @Test
     void testCreatePostFailureByInvalidBoardId() {
+        // given
+        Account account = createAccount();
+        long boardId = 1L;
+        String title = "test-title";
+        String content = "test-content";
+
+        // when
+        // then
+        assertThrows(BoardNotFoundException.class, () -> {
+            postServiceImpl.createPost(account, boardId, title, content, null);
+        });
     }
 
     @DisplayName("게시글 작성 성공 - 이미지 없음")
     @Test
     void testCreatePostSuccessWithoutImage() {
+        // given
+        Account account = createAccount();
+        long boardId = 1L;
+        String title = "test-title";
+        String content = "test-content";
+        Board board = createBoard();
+        given(boardRepository.findById(boardId)).willReturn(Optional.of(board));
+
+        // when
+        postServiceImpl.createPost(account, boardId, title, content, null);
+
+        // then
+        then(postRepository).should().save(any());
     }
 
     @DisplayName("게시글 작성 성공 - 1개의 이미지")
     @Test
-    void testCreatePostSuccessWithImage() {
+    void testCreatePostSuccessWithImage() throws IOException {
+        // given
+        long boardId = 1L;
+        String title = "test-title";
+        String content = "test-content";
+        Account account = createAccount();
+        Board board = createBoard();
+        List<MultipartFile> files = new ArrayList<>();
+        files.add(new MockMultipartFile(
+                "profileImage",
+                "profileImage.jpg",
+                "image/jpeg",
+                new ClassPathResource("/images/profileImage.jpg").getInputStream()
+        ));
+        given(boardRepository.findById(boardId)).willReturn(Optional.of(board));
+
+        // when
+        postServiceImpl.createPost(account, boardId, title, content, files);
+
+        // then
+        then(postRepository).should().save(any());
+        then(attachmentService).should().uploadPostImage(any(), eq(files.get(0)), any());
     }
 
     @DisplayName("게시글 작성 성공 - 복수의 이미지")
     @Test
-    void testCreatePostSuccessWithImages() {
+    void testCreatePostSuccessWithImages() throws IOException {
+        // given
+        long boardId = 1L;
+        String title = "test-title";
+        String content = "test-content";
+        Account account = createAccount();
+        Board board = createBoard();
+        List<MultipartFile> files = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            files.add(
+                    new MockMultipartFile(
+                            String.valueOf(i),
+                            String.valueOf(i),
+                            "image/jpeg",
+                            new ClassPathResource("/images/profileImage.jpg").getInputStream()
+                    )
+            );
+        }
+        given(boardRepository.findById(boardId)).willReturn(Optional.of(board));
+
+        // when
+        postServiceImpl.createPost(account, boardId, title, content, files);
+
+        // then
+        then(postRepository).should().save(any());
+        then(attachmentService).should().uploadPostImages(any(), eq(files), any());
     }
 
     // ----- 게시글 수정 -----
+    @DisplayName("게시글 수정 실패 -  작성자 불일치")
+    @Test
+    void testUpdatePostFailureByAuthorNotEqual() {
+        // given
+        Account account = Account.builder()
+                .id(3L)
+                .email("test@test.com")
+                .password("$2a$10$2H.qwzvH9zq4NrqrGJWdZOVZ4nrx3rfgEqnKvK98fWvaop0ceVtt2")
+                .nickname("test")
+                .role(Role.NOT_PERMITTED)
+                .salt("$2a$10$2H.qwzvH9zq4NrqrGJWdZO")
+                .profileImage(null)
+                .build();
+        Post post = Post.builder()
+                .id(1L)
+                .board(createBoard())
+                .author(createAccount())
+                .title("title1")
+                .content("content1")
+                .attachments(new ArrayList<>())
+                .readCount(0)
+                .heartCount(0)
+                .build();
+        long postId = 1L;
+        String title = "modify-title";
+        String content = "modify-content";
+
+        given(postRepository.findById(postId)).willReturn(Optional.of(post));
+
+        // when
+        // then
+        assertThrows(AuthorNotEqualException.class, () -> {
+            postServiceImpl.updatePost(account, postId, title, content, null);
+        });
+    }
+
     @DisplayName("게시글 수정 성공 - 이미지 없음")
     @Test
     void testUpdatePostSuccessWithoutImage() {
+        // given
+        Account account = createAccount();
+        Post post = createPost();
+        long postId = 1L;
+        String title = "modify-title";
+        String content = "modify-content";
+
+        given(postRepository.findById(postId)).willReturn(Optional.of(post));
+
+        // when
+        postServiceImpl.updatePost(account, postId, title, content, null);
+
+        // then
+        assertEquals(title, post.getTitle());
+        assertEquals(content, post.getContent());
+        then(postRepository).should().save(any());
     }
 
     @DisplayName("게시글 수정 성공 - 이미지 존재")
     @Test
-    void testUpdatePostSuccessWithImages() {
+    void testUpdatePostSuccessWithImages() throws IOException {
+        // given
+        Account account = createAccount();
+        Post post = createPost();
+        long postId = 1L;
+        String title = "modify-title";
+        String content = "modify-content";
+        List<MultipartFile> files = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            files.add(
+                    new MockMultipartFile(
+                            String.valueOf(i),
+                            String.valueOf(i),
+                            "image/jpeg",
+                            new ClassPathResource("/images/profileImage.jpg").getInputStream()
+                    )
+            );
+        }
+        Post savedPost = Post.builder()
+                .id(1L)
+                .board(post.getBoard())
+                .author(post.getAuthor())
+                .title(title)
+                .content(content)
+                .attachments(new ArrayList<>())
+                .readCount(0)
+                .heartCount(0)
+                .build();
+
+        given(postRepository.findById(postId)).willReturn(Optional.of(post));
+        given(postRepository.save(post)).willReturn(savedPost);
+
+        // when
+        postServiceImpl.updatePost(account, postId, title, content, files);
+
+        // then
+        assertEquals(title, post.getTitle());
+        assertEquals(content, post.getContent());
+        then(postRepository).should().save(any());
+        then(attachmentService).should().deletePostImages(post);
+        then(attachmentService).should().uploadPostImages(any(), any(), any());
     }
 
     // ----- 게시글 삭제 -----
-    @DisplayName("게시글 삭제 실패 - postId 존재하지 않음")
+    @DisplayName("게시글 삭제 성공 - 추천, 댓글 존재시 삭제")
     @Test
-    void testDeletePostFailureByInvalidPostId() {
+    void testDeletePostSuccessWithEtc() throws IOException {
+        // given
+        long postId = 1L;
+        Account account = createAccount();
+        Post post = createPost();
+
+        given(postRepository.findById(postId)).willReturn(Optional.of(post));
+        given(heartService.getHeartCountForPost(postId)).willReturn(10L);
+        given(commentService.getCommentCountForPost(postId)).willReturn(10L);
+
+        // when
+        postServiceImpl.deletePost(account, postId);
+
+        // then
+        then(heartService).should().deleteAllHeartForPost(postId);
+        then(commentService).should().deleteAllCommentForPost(postId);
+        then(postRepository).should().deleteById(postId);
     }
 
-    @DisplayName("게시글 삭제 성공 - 추천, 댓글, 이미지 존재시 삭제")
-    @Test
-    void testDeletePostSuccessWithEtc() {
-    }
-
-    @DisplayName("게시글 삭제 성공 - 추천, 댓글, 이미지 없을때 삭제")
+    @DisplayName("게시글 삭제 성공 - 추천, 댓글 없을때 삭제")
     @Test
     void testDeletePostSuccessWithoutEtc() {
-    }
+        // given
+        long postId = 1L;
+        Account account = createAccount();
+        Post post = createPost();
 
+        given(postRepository.findById(postId)).willReturn(Optional.of(post));
+        given(heartService.getHeartCountForPost(postId)).willReturn(0L);
+        given(commentService.getCommentCountForPost(postId)).willReturn(0L);
+
+        // when
+        postServiceImpl.deletePost(account, postId);
+
+        // then
+        then(heartService).should(never()).deleteAllHeartForPost(postId);
+        then(commentService).should(never()).deleteAllCommentForPost(postId);
+        then(postRepository).should().deleteById(postId);
+    }
 }
