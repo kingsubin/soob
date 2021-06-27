b1d9c6e024a743c0e310efaff1b6bf049dedd68d
kingsubin <binch1226@naver.com>
2021-06-27 17:59:35 +0900

    feat: SwaggerConfig content type 수정
    - consumes 추가
    - produces 추가

--- src/main/java/com/community/soob/config/SwaggerConfig.java
+++ src/main/java/com/community/soob/config/SwaggerConfig.java
@@ -6,9 +6,13 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 @EnableSwagger2
@@ -17,6 +21,8 @@ public class SwaggerConfig {
     public Docket swaggerApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(swaggerInfo())
+                .consumes(getConsumeContentTypes())
+                .produces(getProduceContentTypes())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.community.soob"))
                 .paths(PathSelectors.any())
@@ -24,10 +30,25 @@ public class SwaggerConfig {
                 .useDefaultResponseMessages(false);
     }
 
+    private Set<String> getConsumeContentTypes() {
+        Set<String> consumes = new HashSet<>();
+        consumes.add("application/json;charset=UTF-8");
+        consumes.add("application/x-www-form-urlencoded");
+        consumes.add("multipart/form-data");
+        return consumes;
+    }
+
+    private Set<String> getProduceContentTypes() {
+        Set<String> produces = new HashSet<>();
+        produces.add("application/json;charset=UTF-8");
+        return produces;
+    }
+
     private ApiInfo swaggerInfo() {
         return new ApiInfoBuilder()
-                .title("Kingsubin Api Documentation")
+                .title("Soob Api Documentation")
                 .description("API 명세서")
+                .contact(new Contact("kingsubin", "https://kingsubin.tistory.com/", "subin.lee1226@gmail.com"))
                 .version("1.0")
                 .build();
     }
